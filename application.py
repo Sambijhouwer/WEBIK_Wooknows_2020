40803260b50fb2376deda3a17e9f8018fc2473ae
Sambijhouwer <sbijhouwers@gmail.com>
2020-01-15 11:18:43 +0100

    merge

--- application.py
+++ application.py
@@ -28,19 +28,8 @@ GAME_ROOMS = {}
 
 @app.route("/")
 def index():
-    if request.method == 'POST':
-        # Make sure a user put in a username
-        if not request.form.get('username'):
-            return render_template('homepage.html')
-
-        username = request.form.get('username')
-        return render_template("game-lobby.html")
-    
-    else:
-        return render_template('homepage.html')
+    return render_template('homepage.html')
         
-
-
 @app.route('/test')
 def test():
     return render_template("test.html")
