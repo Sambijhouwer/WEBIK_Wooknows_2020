941281b505c8d63601fb013e7ccf4931ca94fea0
Steijn <Steijnwehman@gmail.com>
2020-01-28 13:11:08 +0100

    Fixed double css in lobby.vue

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -269,52 +269,6 @@ export default {
   border-style: solid;
 }
 
-/* Four tiles answers; game lobby */
-.tile.is-child.is-info.box {
-  background-color: #1FB58F;
-  color: white;
-  padding-top: 10%;
-  text-align: center;
-
-  border-color: #168267;
-  border-width: 4px;
-  border-style: solid;
-}
-
-.tile.is-child.is-info.box:hover {
-  filter: brightness(92%);
-}
-
-.tile.is-child.is-primary.box {
-  background-color: #EAB126;
-  color: white;
-  padding-top: 10%;
-  text-align: center;
-
-  border-color: #c2921f;
-  border-width: 4px;
-  border-style: solid;
-}
-
-.tile.is-child.is-primary.box:hover {
-  filter: brightness(92%);
-}
-
-.tile.is-child.is-danger.box {
-  background-color: #F24C4E;
-  color: white;
-  padding-top: 10%;
-  text-align: center;
-
-  border-color: #d13f40;
-  border-width: 4px;
-  border-style: solid;
-}
-
-.tile.is-child.is-danger.box:hover {
-  filter: brightness(92%);
-}
-
 .tile.is-child.is-warning.box {
   background-color: #1B7B34;
   color: white;
